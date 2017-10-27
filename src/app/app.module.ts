974647878cad19e6023f755e97b07852375299dd
coolba73 <coolba73@outlook.com>
2017-10-27 14:25:51 +0900

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { MainComponent  } from "./main/main.component";
+import { AppRouting } from "./app.routing";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { 
+         MatCommonModule 
+        ,MatButtonModule
+        ,MatMenuModule
+       } from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent
+    ,MainComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRouting,
+    BrowserAnimationsModule,
+    MatCommonModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
