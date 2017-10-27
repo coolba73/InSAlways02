1947a86aa06e6e4c052ba1e0679d252091ecfcc4
coolba73 <coolba73@outlook.com>
2017-10-27 19:26:07 +0900

    1

--- src/app/myapp/myapp.module.ts
+++ src/app/myapp/myapp.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from "@angular/core";
 import { MyAppRouting } from "./myapp.routing"; 
 import { FlowTestComponent } from "./testapp/flowtest/flowtest.component";
+import { DrawCanvasComponent } from "./core/material/drawcanvas/drawcanvas.component";
+import { MatCommonModule, MatButtonModule } from "@angular/material";
 
 @NgModule({
     imports:[
         MyAppRouting
+        ,MatCommonModule
+        ,MatButtonModule
     ],
     declarations:[
-        FlowTestComponent
+        FlowTestComponent,
+        DrawCanvasComponent
     ]
 })
 export class MyAppModule{}
--- src/app/myapp/testapp/flowtest/flowtest.component.ts
+++ src/app/myapp/testapp/flowtest/flowtest.component.ts
@@ -1,4 +1,11 @@
-import { Component } from "@angular/core";
+import { Component,ViewChild }           from "@angular/core";
+import { DrawCanvasComponent } from "../../core/material/drawcanvas/drawcanvas.component";
+import { BaseObject }          from "../../core/drawobject/BaseObject";
+import { BoxBase }             from "../../core/drawobject/BoxBase";
+import { FlowBox }             from "../../core/drawobject/FlowBox";
+import { LineBase }            from "../../core/drawobject/LineBase";
+import { SelectBox }           from "../../core/drawobject/SelectBox";
+import { UUID }                from "angular2-uuid";
 
 @Component({
     selector : 'flowtest',
@@ -8,4 +15,20 @@ import { Component } from "@angular/core";
 
 export class FlowTestComponent{
     
-}
+    //________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+    @ViewChild("fcvs") finCanvas : DrawCanvasComponent;
+
+    //________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+    AddFlow(){
+        
+                let flowBox = new FlowBox();
+        
+                flowBox.x = 10;
+                flowBox.y = 10;
+        
+                this.finCanvas.AddObject(flowBox);
+        
+    }
+
+    
+}//class
